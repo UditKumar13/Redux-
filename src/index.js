29dcc45388cfbb86600c679dbb55752cf54fbdcd
Uditkumar13 <uditkumar2401@gmail.com>
2020-07-27 22:22:58 +0530

    Using the redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore,combineReducers,applyMiddleware} from 'redux';
+import {createStore,combineReducers,applyMiddleware,compose} from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -25,7 +25,9 @@ const logger = store =>{
         }
     }
 }
-const store = createStore(rootReducer,applyMiddleware(logger));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)));
 
 
 
